f9209552aa5774a7ebf62cd551a7f483d0ba2351
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-02-14 09:24:51 +0100

    RAZ

--- src/PhpC2/Enums/AdminLevelEnum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\PhpC2\Enums;
-
-enum AdminLevelEnum: string
-{
-    case ADMIN = 'ADMIN';
-    case SUPERADMIN = 'SUPERADMIN';
-}
--- src/PhpC2/Interfaces/AuthInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\PhpC2\Interfaces;
-
-interface AuthInterface
-{
-    public function auth(string $login, string $password): bool;
-}
